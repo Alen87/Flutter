1f3189e4ea906eb8c7fa4661e25e0d9f8298f44b
Alen Boric <boricalen@live.com>
2022-11-28 00:22:24 +0100

    widget_folder

--- projects/personal_expenses_app/lib/main.dart
+++ projects/personal_expenses_app/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:personal_expenses_app/widgets/user_transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -38,6 +39,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
+          UserTransaction()
         ],
       ),
     );
