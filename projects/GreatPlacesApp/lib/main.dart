dd93e0dfe2e07cde26b3165f3c2cf7eec5e22806
Alen Boric <boricalen@live.com>
2023-01-17 01:42:20 +0100

    screens,providers,models

---
+++ projects/GreatPlacesApp/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great Places',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
+            .copyWith(secondary: Colors.amber),
+      ),
+      //home: ...,
+    );
+  }
+}
