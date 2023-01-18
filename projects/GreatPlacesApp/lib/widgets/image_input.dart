486845945e4625e33279da4a39b29533b6189825
Alen Boric <boricalen@live.com>
2023-01-19 00:55:33 +0100

    Using ImagePicker & The Device Camera

--- projects/GreatPlacesApp/lib/widgets/image_input.dart
+++ projects/GreatPlacesApp/lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -9,6 +10,12 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
+
+  Future<void> _takePicture() async {
+    final picker = ImagePicker();
+    final imageFile =
+        await picker.pickImage(source: ImageSource.camera, maxWidth: 600);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +49,7 @@ class _ImageInputState extends State<ImageInput> {
               'Take Picture',
               style: TextStyle(color: Theme.of(context).primaryColor),
             ),
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
